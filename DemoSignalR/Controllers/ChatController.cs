5fce844a122cd1a34d7aa5b32c6c24f31e93373e
unknown <steve.lorent@gmail.com>
2022-05-25 12:29:12 +0200

    chat controller

---
+++ DemoSignalR/Controllers/ChatController.cs
@@ -0,0 +1,24 @@
+﻿using DemoSignalR.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoSignalR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private DataContext _dc;
+
+        public ChatController(DataContext dc)
+        {
+            _dc = dc;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_dc.Messages);
+        }
+    }
+}
